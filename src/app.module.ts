2e8f22bca685c8aaaa7a36c0575cfd6f32eb2f09
Luiz Seixas <guilhermesschiavon@gmail.com>
2025-07-28 22:56:20 -0400

    feat: create user rest resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ContactModule } from './contact/contact.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, ContactModule],
+  imports: [PrismaModule, ContactModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
