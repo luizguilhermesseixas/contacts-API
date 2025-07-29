090b91df16ce10f6936b334d8cecd05dd83e2c7e
Luiz Seixas <guilhermesschiavon@gmail.com>
2025-07-29 01:33:32 -0400

    feat: create contact controller methods

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
 } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { CreateContactDto } from './dto/request/create-contact.dto';
@@ -16,27 +17,34 @@ export class ContactController {
   constructor(private readonly contactService: ContactService) {}
 
   @Post()
-  create(@Body() createContactDto: CreateContactDto) {
-    return this.contactService.create(createContactDto);
+  create(
+    @Body() createContactDto: CreateContactDto,
+    @Req() req: { user: { id: string } },
+  ) {
+    return this.contactService.create(createContactDto, req.user.id);
   }
 
   @Get()
-  findAll() {
-    return this.contactService.findAll();
+  findAll(@Req() req: { user: { id: string } }) {
+    return this.contactService.findAll(req.user.id);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.contactService.findOne(+id);
+  findOne(@Param('id') id: string, @Req() req: { user: { id: string } }) {
+    return this.contactService.findOne(id, req.user.id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
-    return this.contactService.update(+id, updateContactDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateContactDto: UpdateContactDto,
+    @Req() req: { user: { id: string } },
+  ) {
+    return this.contactService.update(id, updateContactDto, req.user.id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.contactService.remove(+id);
+  remove(@Param('id') id: string, @Req() req: { user: { id: string } }) {
+    return this.contactService.remove(id, req.user.id);
   }
 }
--- src/contact/dto/request/create-contact.dto.ts
+++ src/contact/dto/request/create-contact.dto.ts
@@ -1,17 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class CreateContactDto {
+  @ApiProperty({ example: 'John' })
   @IsString()
   firstName: string;
 
+  @ApiProperty({ example: 'Doe' })
   @IsString()
   lastName: string;
 
+  @ApiProperty({ example: 'john.doe@example.com' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: '+1234567890' })
   @IsString()
   phone: string;
 }
