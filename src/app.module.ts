17188a2287a0be23163ded8808e0177b6718216c
Luiz Seixas <guilhermesschiavon@gmail.com>
2025-07-28 22:59:11 -0400

    feat: create service, controller and module for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ContactModule } from './contact/contact.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ContactModule, UserModule],
+  imports: [PrismaModule, ContactModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
