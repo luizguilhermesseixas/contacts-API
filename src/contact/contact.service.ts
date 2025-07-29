12601b5ec3699c7383200546e622cebb81ca38f4
Luiz Seixas <guilhermesschiavon@gmail.com>
2025-07-29 01:04:15 -0400

    feat: create contact service methods

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -7,7 +7,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class ContactService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createContactDto: CreateContactDto, userId: string) {
+  async create(createContactDto: CreateContactDto, userId: string) {
     return this.prisma.contact.create({
       data: {
         ...createContactDto,
@@ -16,19 +16,38 @@ export class ContactService {
     });
   }
 
-  findAll() {
-    return `This action returns all contact`;
+  async findAll(userId: string) {
+    return this.prisma.contact.findMany({
+      where: { userId },
+      orderBy: { createdAt: 'desc' },
+    });
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} contact`;
+  async findOne(id: string, userId: string) {
+    const contact = await this.prisma.contact.findFirst({
+      where: { id, userId },
+    });
+
+    if (!contact) {
+      throw new Error(`Contact with id ${id} not found`);
+    }
+    return contact;
   }
 
-  update(id: string, updateContactDto: UpdateContactDto) {
-    return `This action updates a #${id} contact`;
+  async update(id: string, updateContactDto: UpdateContactDto, userId: string) {
+    await this.findOne(id, userId);
+
+    return this.prisma.contact.update({
+      where: { id },
+      data: updateContactDto,
+    });
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} contact`;
+  async remove(id: string, userId: string) {
+    await this.findOne(id, userId);
+
+    return this.prisma.contact.delete({
+      where: { id },
+    });
   }
 }
