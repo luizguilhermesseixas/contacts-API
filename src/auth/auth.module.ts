0082c87c8acc15bfb04052ec737f539c4acb0af0
Luiz Seixas <guilhermesschiavon@gmail.com>
2025-07-29 20:11:34 -0400

    feat: setup JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'SECRET_KEY',
+      signOptions: { expiresIn: '1h' },
+    }),
+    PrismaModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
+  exports: [AuthService],
 })
 export class AuthModule {}
